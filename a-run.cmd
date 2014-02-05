7f63f52afa2f067c9eb253b6ebe17f4b56ad03ba
Jeremy Ellis <jellis@hpssapps.com>
2014-02-04 23:37:20 -0800

    Create a-run.cmd

---
+++ a-run.cmd
@@ -0,0 +1,14 @@
+REM use ctrl-F for file autofill
+REM use ctrl-D for directory autofill
+REM use dir/x  for short files names
+REM use PATH    to check your system path
+REM use android -h  to check Android path
+REM use ant -V  (captial V) to check ant path
+REM use git --version  to check git path
+REM use node - v (small v) to check node path
+REM use phonegap -v (small v) to check phonegap path
+REM use plugman -v  (small v) to check plugman path
+REM
+
+
+cmd /f
