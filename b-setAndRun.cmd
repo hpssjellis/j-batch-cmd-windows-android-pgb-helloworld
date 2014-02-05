5b7b752597740ad169ef4454ad19e9234c7033aa
Jeremy Ellis <jellis@hpssapps.com>
2014-02-04 23:38:08 -0800

    Create b-setAndRun.cmd

---
+++ b-setAndRun.cmd
@@ -0,0 +1,29 @@
+
+set ANT_HOME=C:\phonegapmain\ant
+set ANDROID_HOME=C:\phonegapmain\androidDec2013\sdk
+set ANDROID_SDK_HOME=%USERPROFILE%
+set GIT_HOME=C:\Progra~1\Git
+set JAVA_HOME=C:\phonegapmain\java
+set NODE_HOME=C:\Users\Kids\Documents\Jeremy\2013_2014\nodejs\node
+set NPM_HOME=c:\users\kids\AppData\Roaming\npm
+
+set PATH=C:\Windows;C:\Windows\System32;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%GIT_HOME%\bin;%GIT_HOME%\cmd;%JAVA_HOME%\bin;%NODE_HOME%;%NPM_HOME%;
+
+set CLASSPATH=
+
+REM use ctrl-F for file autofill
+REM use ctrl-D for directory autofill
+REM use dir/x  for short files names
+REM use PATH    to check your system path
+REM use android -h  to check Android path
+REM use ant -V  (captial V) to check ant path
+REM use git --version  to check git path
+REM use node - v (small v) to check node path
+REM use phonegap -v (small v) to check phonegap path
+REM use plugman -v  (small v) to check plugman path
+REM
+
+path
+cmd /f
+
+
