1966beba7a44fd268979f861b7d7f4438f69016b
Jeremy Ellis <jellis@hpssapps.com>
2014-05-06 23:05:42 -0700

    Update b-setAndRun.cmd

--- b-setAndRun.cmd
+++ b-setAndRun.cmd
@@ -3,6 +3,7 @@ REM use ctrl-F for file autofill
 REM use ctrl-D for directory autofill
 REM use dir/x  for short files names
 REM use PATH    to check your system path
+REM use java -version to check java path
 REM use android -h  to check Android path
 REM use ant -V  (captial V) to check ant path
 REM use git --version  to check git path
@@ -15,15 +16,16 @@ REM DO  NOT CALL THIS BATCH FILE cmd.bat or command.bat or cmd.cmd or command.cm
 
 
 
-set ANT_HOME=C:\phonegapmain\ant
-set ANDROID_HOME=C:\phonegapmain\androidDec2013\sdk
+set ANT_HOME=C:\Program Files\ant
+set ANDROID_HOME=C:\android\sdk
 set ANDROID_SDK_HOME=%USERPROFILE%
-set GIT_HOME=C:\Progra~1\Git
-set JAVA_HOME=C:\phonegapmain\java
-set NODE_HOME=C:\Users\myComp\Documents\myFolder\nodejs\node
-set NPM_HOME=c:\users\myComp\AppData\Roaming\npm
+set GIT_HOME=C:\Program Files\Git
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_05
+set NODE_HOME=C:\Program Files\nodejs
+set NPM_HOME=C:\Program Files\nodejs\node_modules\npm
+set PHONEGAP_HOME=C:\pg34z\node_modules\.bin
 
-set PATH=C:\Windows;C:\Windows\System32;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%GIT_HOME%\bin;%GIT_HOME%\cmd;%JAVA_HOME%\bin;%NODE_HOME%;%NPM_HOME%;
+set PATH=C:\Windows;C:\Windows\System32;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%GIT_HOME%\bin;%GIT_HOME%\cmd;%JAVA_HOME%\bin;%NODE_HOME%;%NPM_HOME%;%PHONEGAP_HOME%;
 
 set CLASSPATH=
 
